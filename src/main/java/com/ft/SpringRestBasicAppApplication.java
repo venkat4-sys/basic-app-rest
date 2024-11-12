3979e4aa7bf3fdaa87df2ffe00221532f24ba1da
Venkat <venkatkilari5@gmail.com>
2024-11-12 05:49:01 -0800

    intial commit

---
+++ src/main/java/com/ft/SpringRestBasicAppApplication.java
@@ -0,0 +1,13 @@
+package com.ft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestBasicAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRestBasicAppApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/ft/rest/WelcomeRestController.java
@@ -0,0 +1,14 @@
+package com.ft.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeRestController {
+	
+	@GetMapping("/msg")
+	public String getWelcomeMsg() {
+		return "welcome to springboot lectures";
+	}
+
+}
